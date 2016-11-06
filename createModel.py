09d2baeddcf7ac59611225c43e4f8a61b2fa3d47
Daniel Lagrava <daniel.lagrava@canterbury.ac.nz>
2016-11-07 08:48:31 +1300

    Some cleaning around the code

--- createModel.py
+++ createModel.py
@@ -54,13 +54,6 @@ def create_dashpot_nodes(y_nodes):
     x_nodes = 2*y_nodes
     increment = (y_max-y_min)/(y_nodes)
     id = len(effective_nodes) + 1
-    # left
-    y_0 = y_min
-    x_0 = x_min
-    for y in range(y_nodes+1):
-        dashpot_nodes_left.append({'id':id,'x':x_0,'y':y_0})
-        y_0 += increment
-        id += 1
 
     y_0 = y_min
     x_0 = x_min
@@ -69,6 +62,16 @@ def create_dashpot_nodes(y_nodes):
         dashpot_nodes_bottom.append({'id':id,'x':x_0,'y':y_0})
         x_0 += increment
         id += 1
+
+    # left
+    y_0 = y_min
+    x_0 = x_min
+
+    for y in range(y_nodes+1):
+        dashpot_nodes_left.append({'id':id,'x':x_0,'y':y_0})
+        y_0 += increment
+        id += 1
+
 
     # right
     y_0 = y_min
@@ -83,6 +86,23 @@ def create_dashpot_elements(y_nodes):
     increment = (y_max-y_min)/(y_nodes)
     id = len(effective_elements) + 1
 
+    # bottom
+    counter = 1
+    for node in dashpot_nodes_bottom:
+        mat = '2 3'
+        if counter == 1 or counter == x_nodes+1:
+            mat = '4 5'
+
+        # find the node id of the normal node that corresponds to that node
+        neighbor = -1
+        for n in effective_nodes:
+            if n['x'] == node['x'] and n['y'] == node['y']:
+                neighbor = n['id']
+
+        dashpot_elements_bottom.append({'node2':node['id'],'node1':neighbor,'id':id,'mat':mat})
+        counter += 1
+        id += 1
+
     # left
     counter = 1
     for node in dashpot_nodes_left:
@@ -94,23 +114,6 @@ def create_dashpot_elements(y_nodes):
         id += 1
         counter += 1
 
-    # bottom
-    counter = 1
-    for node in dashpot_nodes_bottom:
-        mat = '2 3'
-        if counter == 1 or counter == x_nodes+1:
-            mat = '4 5'
-
-        # find the node id of the normal node that corresponds to that node
-        neighber = -1
-        for n in effective_nodes:
-            if n['x'] == node['x'] and n['y'] == node['y']:
-                neighbor = n['id']
-
-        dashpot_elements_bottom.append({'node2':node['id'],'node1':neighbor,'id':id,'mat':mat})
-        counter += 1
-        id += 1
-
     # right
     counter = 1
     for node in dashpot_nodes_right:
@@ -119,7 +122,7 @@ def create_dashpot_elements(y_nodes):
             mat = '5 4'
 
         # find the node id of the normal node that corresponds to that node
-        neighber = -1
+        neighbor = -1
         for n in effective_nodes:
             if n['x'] == node['x'] and n['y'] == node['y']:
                 neighbor = n['id']
@@ -133,6 +136,7 @@ def find_middle_point():
     for node in effective_nodes:
         if node['x'] == 0.0 and node['y'] == 0.0:
             return node['id']
+    return -1
 
 def resolve_template():
     j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
@@ -159,6 +163,9 @@ if __name__ == '__main__':
     create_dashpot_nodes(y_elements)
     create_dashpot_elements(y_elements)
     middle_point = find_middle_point()
+    if middle_point == -1:
+        print "Could not find middle point"
+        exit(1)
     resolve_template()
 
 
